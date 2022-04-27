7a771ccef219903f7f6b1ca9608f10da01280952
ferielll <feriel.hamrouni@esprit.tn>
2022-04-27 07:56:26 +0100

    feriel changes 04/27/2022

--- server/models/comment.js
+++ server/models/comment.js
@@ -5,6 +5,7 @@ const CommentSchema = new mongoose.Schema({
     postowner: { type: mongoose.Schema.Types.ObjectId, ref: 'User'},
     post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post'},
     responseTo : { type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
 }, {timestamps:true},{versionKey:false})
 
 
